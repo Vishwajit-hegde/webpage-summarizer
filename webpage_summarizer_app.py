9308be40b77d5e7943696a7bfc7bf4ba591f38bc
vishwajit-hegde <38160770+Vishwajit-hegde@users.noreply.github.com>
2024-08-16 10:35:25 +0530

    Update webpage_summarizer_app.py

--- webpage_summarizer_app.py
+++ webpage_summarizer_app.py
@@ -14,14 +14,12 @@ st.title("Webpage Summarizer")
 with open("api_key.txt", "r") as f:
     encrypted_api = f.read()
 password = st.text_input("Enter password",type='password')
-x = 1
-try:
-    API_KEY = decrypt_api(password, encrypted_api)
-    CLIENT = Fireworks(api_key=API_KEY)
-except:
-    st.markdown('Incorrect password')
-    x = 0
+model_dict = {'Llama3.1-8B Instruct':"accounts/fireworks/models/llama-v3p1-8b-instruct",
+              'Llama3.1-70B Instruct':"accounts/fireworks/models/llama-v3p1-70b-instruct",
+              'Mixtral MoE 8x22B Instruct':"accounts/fireworks/models/mixtral-8x22b-instruct"}
 
+model = st.selectbox(label="Select a model",options=list(model_dict.keys()),index=0)
+model_id = model_dict[model]
 
 def get_response_text_from_url(url):
     if url.startswith('https://')==False and url.startswith('http://')==False:
@@ -77,25 +75,27 @@ def get_llm_response(llm_prompt,model_id="accounts/fireworks/models/llama-v3p1-8
                                             )
     return response.choices[0].message.content
 
-
-
-if x:
-    url = st.text_input("Enter Webpage URL")
-    query = st.text_input("Enter your query")
-
-    submit = st.button("Summarize")
-    if submit and url!='' and query!='':
+url = st.text_input("Enter Webpage URL")
+query = st.text_input("Enter your query")
+submit = st.button("Summarize")
+if submit and query!='' and url!='' and password!='':
+    try:
+        API_KEY = decrypt_api(password, encrypted_api)
+        CLIENT = Fireworks(api_key=API_KEY)
         web_content = get_response_text_from_url(url)
-        st.markdown("Text extraction done")
+        #st.markdown("Text extraction done")
         if web_content=='no response':
             st.markdown("There is no response from the webpage. Please enter another URL")
         else:
             web_content = process_text(web_content)
             chunks = text_splitter(web_content)
-            st.markdown("Chunking done")
+            #st.markdown("Chunking done")
             EMBED_MODEL = SentenceTransformer("mixedbread-ai/mxbai-embed-large-v1")
             context = retrieve_topn_docs(chunks,query,n=1)
-            st.markdown("Embedding done")
+            #st.markdown("Embedding done")
             llm_prompt = create_llm_prompt(context=context,query=query)
-            llm_output = get_llm_response(llm_prompt=llm_prompt)
+            llm_output = get_llm_response(llm_prompt=llm_prompt,model_id=model_id)
             st.markdown(llm_output)
+    except:
+        st.markdown('Incorrect password')
+ 
